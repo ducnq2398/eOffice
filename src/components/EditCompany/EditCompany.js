9bee26f74e62959dd847f5f3d3aa2bf2edda760c
Đức <44558442+quangduc0203@users.noreply.github.com>
2021-01-23 22:26:39 +0700

    finish project

--- src/components/EditCompany/EditCompany.js
+++ src/components/EditCompany/EditCompany.js
@@ -2,28 +2,17 @@ import React, { useState } from 'react';
 import logo from './../../images/logo.png';
 import './EditCompany.css'
 import {useHistory} from 'react-router-dom';
-import {Button, Col, Container, Form, FormGroup, Input, Label, Modal, ModalHeader,ModalBody,ModalFooter} from 'reactstrap'
+import {Button, Col, Container, Form, FormGroup, Input, Label} from 'reactstrap'
+import ValidateCompany from '../Validation/ValidateCompany';
+import ValidatePhone from '../Validation/ValidatePhone';
+import ValidateEmail from '../Validation/ValidateEmail';
+import ValidateName from '../Validation/ValidateName';
+import ValidateDepartment from '../Validation/ValidateDepartment';
+import ValidateAddress from '../Validation/ValidateAddress';
 
-const validateInput = (checkingText) => {
-    /* reg exp để kiểm tra xem chuỗi có chỉ bao gồm 10 - 11 chữ số hay không */
-
-    const regexp = /^\d{10}$/;
-    const checkingResult = regexp.exec(checkingText);
-    if (checkingResult !== null) {
-        return { isInputValid: true,
-                 errorMessage: ''};
-    } else {
-        return { isInputValid: false,
-                 errorMessage: 'Phone number is incorrect'};
-    }
-}
-
-function CompanyRegister(props){
+function CompanyRegister(){
     const history = useHistory();
     const home = () => history.push('/admin-manager');
-    const {
-        className
-      } = props;
     const [modal, setModal] = useState(false);
     const toggle = () => setModal(!modal);
     const [company_name, setCompanyName] = useState('');
@@ -32,24 +21,78 @@ function CompanyRegister(props){
     const [applicant, setApplicant] = useState('');
     const [applicant_department, setApplicantDepartment] = useState('');
     const [applicant_email, setApplicantEmail] = useState('');
-    const [isInputValid, setIsInputInvalid] = useState(true);
-    const [errorMessage, setErrorMessage] = useState('');
+    const [validPhone, setValidPhone] = useState({
+        isValid: false,
+        isInValid: false
+    });
+    const [validCompany, setValidCompany] = useState({
+        isValid: false,
+        isInValid: false
+    });
+    const [validAddress, setValidAddress] = useState({
+        isValid: false,
+        isInValid: false
+    });
+    const [validName, setValidName] = useState({
+        isValid: false,
+        isInValid: false
+    });
+    const [validDepartment, setValidDepartment] = useState({
+        isValid: false,
+        isInValid: false
+    });
+    const [validEmail, setValidEmail] = useState({
+        isValid: false,
+        isInValid: false
+    });
 
-    
-
-    function handleInputValidation(event){
-        const { isInputValid, errorMessage } = validateInput(getPhone);
-        setIsInputInvalid(isInputValid);
-        setErrorMessage(errorMessage);
+    function checkEmail() {
+        const {isValid, isInValid} = ValidateEmail(applicant_email);
+        setValidEmail({
+            isValid: isValid,
+            isInValid: isInValid
+        })
     }
 
-
-    function FormError(props) {
-        /* nếu isHidden = true, return null ngay từ đầu */
-        if (props.isHidden) { return null;}
-    
-        return ( <div className="form-warning">{props.errorMessage}</div>)
+    function checkPhone(){
+        const {isValid, isInValid} = ValidatePhone(phone_number);
+        setValidPhone({
+            isValid: isValid,
+            isInValid: isInValid
+        })
     }
+
+    function checkCompany() {
+        const {isValid, isInValid} = ValidateCompany(company_name);
+        setValidCompany({
+            isValid: isValid,
+            isInValid: isInValid
+        })
+    }
+
+    function checkAddress() {
+        const {isValid, isInValid} = ValidateAddress(street_address);
+        setValidAddress({
+            isValid: isValid,
+            isInValid: isInValid
+        })
+    }
+
+    function checkName() {
+        const{isValid , isInValid} = ValidateName(applicant);
+        setValidName({
+            isValid: isValid,
+            isInValid: isInValid
+        })
+    }
+    function checkDepartment() {
+        const{isValid , isInValid} = ValidateDepartment(applicant_department);
+        setValidDepartment({
+            isValid: isValid,
+            isInValid: isInValid
+        })
+    }
+
     
     
     function getCompanyName(event){
@@ -88,32 +131,31 @@ function CompanyRegister(props){
                         <FormGroup row>
                             <Label sm={2}>Company Name</Label>
                             <Col sm={8}>
-                                <Input type="text" name="company_name" required="required" onChange={getCompanyName}/>
+                                <Input valid={validCompany.isValid} invalid={validCompany.isInValid} onBlur={checkCompany} type="text" name="company_name" required="required" onChange={getCompanyName}/>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label sm={2}>Street Address</Label>
                             <Col sm={8}>
-                                <Input type="text" name="street_address" onChange={getStreetAddress}/>
+                                <Input valid={validAddress.isValid} invalid={validAddress.isInValid} onBlur={checkAddress} type="text" name="street_address" onChange={getStreetAddress}/>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label sm={2}>Phone Number</Label>
                             <Col sm={8}>
-                                <Input type="text" name="phone_number" required="required" onChange={getPhone} onBlur={handleInputValidation}/>
-                                <FormError isHidden={isInputValid} errorMessage={errorMessage} />
+                                <Input valid={validPhone.isValid} invalid={validPhone.isInValid} onBlur={checkPhone} type="text" name="phone_number" required="required" onChange={getPhone}/>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label sm={2}>Applicant</Label>
                             <Col sm={8}>
-                                <Input type="text" name="applicant" required="required" onChange={getApplicant}/>
+                                <Input valid={validName.isValid} invalid={validName.isInValid} onBlur={checkName} type="text" name="applicant" required="required" onChange={getApplicant}/>
                             </Col>
                         </FormGroup>
                         <FormGroup row>
                             <Label sm={2}>Applicant Department</Label>
                             <Col sm={8}>
-                                <Input type="select" name="applicant_department" id="exampleSelect" onChange={getDepartment}>
+                                <Input valid={validDepartment.isValid} invalid={validDepartment.isInValid} onBlur={checkDepartment} type="select" name="applicant_department" id="exampleSelect" onChange={getDepartment}>
                                     <option>Production</option>
                                     <option>Purchasing</option>
                                     <option>Marketing</option>
@@ -125,63 +167,29 @@ function CompanyRegister(props){
                         <FormGroup row>
                             <Label sm={2}>Applicant Email</Label>
                             <Col sm={8}>
-                                <Input type="email" name="applicant_email" required="required" onChange={getEmail}/>
+                                <Input valid={validEmail.isValid} invalid={validEmail.isInValid} onBlur={checkEmail} type="email" name="applicant_email" required="required" onChange={getEmail}/>
                             </Col>
+                        </FormGroup>
+                        <FormGroup tag="fieldset" row>
+                            <Label sm={2}>Status</Label>
+                            <FormGroup check>
+                                <Label check sm={4}>
+                                    <Input type="radio" name="active"/>
+                                    Active
+                                </Label>
+                            </FormGroup>
+                            <FormGroup check>
+                                <Label check sm={4}>
+                                    <Input type="radio" name="deactive"/>
+                                    Deactive
+                                </Label>
+                            </FormGroup>
                         </FormGroup>
                         <FormGroup>
                             <Button style={{padding:'15px 10px 15px',width:'400px',marginLeft:'auto', marginRight:'auto',marginTop:'20px'}} color="primary" size="lg" block onClick={toggle}>Save</Button>
                         </FormGroup>
                     </Form>
                 </Container>
-
-                <Modal isOpen={modal} toggle={toggle} className={className}>
-                    <ModalHeader toggle={toggle}>Confirm Information</ModalHeader>
-                    <ModalBody>
-                        <Form>
-                            <FormGroup row>
-                                <Label>Company Name:</Label>
-                                <Col sm={6}>
-                                    {company_name}
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label>Street Address:</Label>
-                                <Col sm={6}>
-                                    {street_address}
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label>Phone Number:</Label>
-                                <Col sm={6}>
-                                    {phone_number}
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label>Applicant:</Label>
-                                <Col sm={6}>
-                                    {applicant}
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label>Applicant Department:</Label>
-                                <Col sm={6}>
-                                    {applicant_department}
-                                </Col>
-                            </FormGroup>
-                            <FormGroup row>
-                                <Label>Applicant Email:</Label>
-                                <Col sm={6}>
-                                    {applicant_email}
-                                </Col>
-                            </FormGroup>
-                        </Form>
-                    </ModalBody>
-                    <ModalFooter>
-                    <Button color="primary" onClick={toggle}>Create</Button>
-                    <Button color="secondary" onClick={toggle}>Cancel</Button>
-                    </ModalFooter>
-                </Modal>
-
             </div>
         );
     }
